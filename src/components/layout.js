46a8f4e0f95f3602ec16a8b56236ff2a6d3a7cec
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-06-16 01:05:20 -0600

    Initial commit even though the app is advance

---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from './headernav/header';
+import Navbar from './headernav/navbar';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className='layout'>
+        {...this.props.children}
+        <Header/>
+        <Navbar/>
+      </div>
+    );
+  }
+}
+
+export default Layout;
