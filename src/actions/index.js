0c80d390495b72bb6a0079c367cba1b85e14d84f
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-06-25 00:23:35 -0600

    refactored navbar to take additional onclick

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,7 +12,8 @@ import {
 
 import {
     fetchShopCategories,
-    fetchShopProducts
+    fetchShopProducts,
+    filterProductsWithCategoryId
 } from './shop'
 
 export {
@@ -24,5 +25,6 @@ export {
     setPurchaseDetail,
 
     fetchShopCategories,
-    fetchShopProducts
+    fetchShopProducts,
+    filterProductsWithCategoryId
 };
