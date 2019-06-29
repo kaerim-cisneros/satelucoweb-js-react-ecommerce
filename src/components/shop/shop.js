f446c9830907c9922c4ed88d2e169cd8ccaa69cc
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-06-28 23:32:17 -0600

    structured shop cart component and styled

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,9 +4,18 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
 
+    constructor() {
+        super()
+
+        this.state = {
+            showCart: true
+        }
+    }
+    
     componentDidMount(){
         const headerLinks = [
             {
@@ -35,6 +44,8 @@ class Shop extends Component {
     
 
     render() {
+        return <ShopCart className='shop__cart'/>
+        
         return (
             <div className='shop'>
                 <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
